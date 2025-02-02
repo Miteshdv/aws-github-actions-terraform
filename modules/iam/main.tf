47f0e621dd860016e6392242596502390a1d3c65
Mitesh Dave <Mitesh.Dave@mphasis.com>
2025-02-02 12:21:51 -0600

    Adding API gateway for lambda

---
+++ modules/iam/main.tf
@@ -0,0 +1,47 @@
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# IAM Policies
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_exec.name
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.project_name}-lambda-logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
---
+++ modules/lambda/variables.tf
@@ -0,0 +1,13 @@
+variable "project_name" {
+  type = string
+}
+
+
+variable "role_arn" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
